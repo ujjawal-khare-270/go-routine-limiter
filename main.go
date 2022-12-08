7b2cf0b73e63876a163bf9a9627fc7ef18f6a474
Ujjawal Khare <85036705+ujjawal-khare-270@users.noreply.github.com>
2022-12-08 23:06:45 +0530

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import (
 	"time"
 )
 
+type GoLimiter struct {
+	fn              func()
+	push            func(arg interface{})
+	maximumRoutines int
+}
+
 var queue = make([]int, 0)
 
 func goLimiter(limit int, isDone chan bool) {
@@ -35,9 +41,26 @@ func ApiCall(i int, ch chan bool) {
 	ch <- true
 }
 
+func pushToQueue(arg interface{}) {
+	queue = queue.append(arg)
+}
+
+func NewGoRoutineLimiter(maximumRoutines int, processor func()) GoLimiter {
+	var queue = make([]interface{}, 0)
+
+	return GoLimiter{
+		push:            pushToQueue,
+		maximumRoutines: maximumRoutines,
+		fn:              processor,
+	}
+
+}
+
 func main() {
 	isDone := make(chan bool)
 	queue = append(queue, 3)
 	queue = append(queue, 3)
+	queue = append(queue, 2)
+
 	goLimiter(1, isDone)
 }
